5a6aca891fcb4f1924646a4652b5f770aac294d8
Maksim <inquisitor914@gmail.com>
2022-12-28 19:27:17 +0300

    Добавьте файлы проекта.

---
+++ CommandModule.cs
@@ -0,0 +1,53 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace BartenderBot
+{
+    internal class CommandModule : BaseCommandModule
+    {
+        private readonly string[] beer = { "Листолюбское особое", "Жирный олух", "Крушитель глифидов", "Аркенстаут", "Усладу Камнестрела", "Андерхилл делюкс", "Сногсшибающий стаут" };
+        private readonly ulong bar = 1056273877719851008;
+
+        [Command("налей")]
+        public async Task BeerCommand(CommandContext ctx)
+        {
+            if ((ctx.Channel.Id == bar) && (!ctx.Message.Author.IsBot))
+            {
+                string author = ctx.Message.Author.Mention;
+                Random random = new();
+                Console.WriteLine(beer.Length);
+                int order = random.Next(beer.Length);
+                if (order == 0)
+                {
+                    await ctx.RespondAsync("*Не шёпотом:* " + author + " заказывает " + beer[order] + "! Повторяю, " + beer[order] + ". Налетайте!");
+                }
+                else
+                {
+                    await ctx.RespondAsync(author + " заказывает " + beer[order] + " для всех!");
+                }
+            } 
+        }
+        [Command("налей")]
+        public async Task BeerCommand(CommandContext ctx, DiscordUser victim)
+        {
+            if ((ctx.Channel.Id == bar) && (!ctx.Message.Author.IsBot))
+            {
+                string author = ctx.Message.Author.Mention;
+                Random random = new();
+                Console.WriteLine(beer.Length);
+                int order = random.Next(beer.Length);
+                if (order == 0)
+                {
+                    await ctx.RespondAsync("*Не шёпотом:* " + author + " заказывает " + beer[order] + "! " + victim.Mention + ", наслаждайся вкусом!");
+                }
+                else
+                {
+                    await ctx.RespondAsync(author + " заказывает " + beer[order] + " для " + victim.Mention + "!");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+
+namespace BartenderBot
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            DiscordConfiguration config = new();
+            config.Token = System.IO.File.ReadAllText("token.txt");
+            config.MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug;
+            config.Intents = DiscordIntents.All;
+            DiscordClient discord = new(config);
+
+            CommandsNextConfiguration commandsConfig = new();
+            commandsConfig.StringPrefixes = new[] { "!" };
+            CommandsNextExtension commands = discord.UseCommandsNext(commandsConfig);
+
+            commands.RegisterCommands<CommandModule>();
+
+            await discord.ConnectAsync();
+
+            await Task.Delay(Timeout.Infinite);
+        }
+    }
+}
