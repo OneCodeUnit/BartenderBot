397985d296f9b6140de595d449164595727d709b
Maksim <inquisitor914@gmail.com>
2023-01-05 15:14:16 +0300

    новая команда

--- CommandModule.cs
+++ CommandModule.cs
@@ -9,43 +9,70 @@ namespace BartenderBot
     internal class CommandModule : BaseCommandModule
     {
         private readonly string[] beer = { "Листолюбское особое", "Жирный олух", "Крушитель глифидов", "Аркенстаут", "Усладу Камнестрела", "Андерхилл делюкс", "Сногсшибающий стаут" };
-        private readonly ulong bar = 1056273877719851008;
-        //private readonly ulong bar = 1056561551638802513; //test
-
+        private readonly ulong bar = Convert.ToUInt64(System.IO.File.ReadAllText("bar.txt"));
+        
         [Command("налей")]
         public async Task BeerCommand(CommandContext ctx)
         {
-            if ((ctx.Channel.Id == bar) && (!ctx.Message.Author.IsBot))
+            if (ctx.Channel.Id == bar)
             {
                 string author = ctx.Message.Author.Mention;
                 Random random = new();
                 int order = random.Next(beer.Length);
+                string answer;
                 if (order == 0)
                 {
-                    await ctx.RespondAsync("*Не шёпотом:* " + author + " заказывает " + beer[order] + "! Повторяю, " + beer[order] + ". Налетайте!");
+                    answer = "*Не шёпотом:* " + author + " заказывает " + beer[order] + "! Повторяю, " + beer[order] + ". Налетайте!";
                 }
                 else
                 {
-                    await ctx.RespondAsync(author + " заказывает " + beer[order] + " для всех!");
+                    answer = author + " заказывает " + beer[order] + " для всех!";
                 }
+                await ctx.RespondAsync(answer);
             } 
         }
         [Command("налей")]
-        public async Task BeerCommand(CommandContext ctx, DiscordUser victim)
+        public async Task BeerCommand(CommandContext ctx, DiscordUser target)
         {
-            if ((ctx.Channel.Id == bar) && (!ctx.Message.Author.IsBot))
+            if (ctx.Channel.Id == bar)
             {
                 string author = ctx.Message.Author.Mention;
                 Random random = new();
                 int order = random.Next(beer.Length);
+                string answer;
                 if (order == 0)
                 {
-                    await ctx.RespondAsync("*Не шёпотом:* " + author + " заказывает " + beer[order] + "! " + victim.Mention + ", наслаждайся вкусом!");
+                    answer = "*Не шёпотом:* " + author + " заказывает " + beer[order] + "! " + target.Mention + ", наслаждайся вкусом!";
                 }
                 else
                 {
-                    await ctx.RespondAsync(author + " заказывает " + beer[order] + " для " + victim.Mention + "!");
+                    answer = author + " заказывает " + beer[order] + " для " + target.Mention + "!";
                 }
+                await ctx.RespondAsync(answer);
+            }
+        }
+        [Command("налей")]
+        public async Task BeerCommand(CommandContext ctx, string target)
+        {
+            if (ctx.Channel.Id == bar)
+            {
+                string author = ctx.Message.Author.Mention;
+                Random random = new();
+                int order = random.Next(beer.Length);
+                string answer = "";
+                if (target != "мне")
+                {
+                    answer += "Слова " + author + "сложно разобрать, но, похоже, ";
+                }
+                if (order == 0)
+                {
+                    answer += "*кхе-кхе* " + author + " заказывает " + beer[order] + " и выпивает... всё... сам.";
+                }
+                else
+                {
+                    answer += author + " заказывает " + beer[order] + " и всё выпивает сам.";
+                }
+                await ctx.RespondAsync(answer);
             }
         }
     }
